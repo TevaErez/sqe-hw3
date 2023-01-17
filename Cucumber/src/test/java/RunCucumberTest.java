7352c00f58861263d7d397a4d14a109ff9a5e95e
TevaErez <teva.erez@gmail.com>
2023-01-17 13:44:19 +0200

    initialization for opencart

---
+++ Cucumber/src/test/java/RunCucumberTest.java
@@ -0,0 +1,49 @@
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("hellocucumber")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+
+    private String ADMIN_USERNAME = "admin";
+    private String ADMIN_PASSWORD = "3322";
+//    private String COURSE_NAME = "Demo course";
+//    private String ADMIN_USERNAME = "admin";
+//    private String ADMIN_PASSWORD = "3322";
+//    private int DEFAULT_LENGTH = 5;
+    private OpenCartActuator opencart;
+    private String webDriver = "webdriver.chrome.driver";
+    private String path = "C:\\Users\\User\\Desktop\\ass3sqe\\sqe-hw3\\Selenium\\chromedriver.exe";
+
+    public RunCucumberTest() {
+        System.out.println("--------------- INITIALIZING MOODLE TEST - OPENING WEBPAGE ---------------");
+        opencart = new OpenCartActuator();
+        opencart.initSession(webDriver, path);
+    }
+
+    public void addCommentTest() {
+        System.out.println("--------------- STARTING MOODLE MULTIPLE CHOICE QUIZ TEST ---------------");
+        System.out.println("--------------- MULTIPLE CHOICE QUIZ TEST - TEACHER LOGIN ---------------");
+//        opencart.addcomment()
+//        opencart.loginSequence(ADMIN_USERNAME, ADMIN_PASSWORD);
+//
+//        System.out.println("--------------- MULTIPLE CHOICE QUIZ TEST - NAVIGATING TO COURSE ---------------");
+//        opencart.myCoursesTab();
+//        opencart.goToCourse(COURSE_NAME);
+//
+//        System.out.println("--------------- MULTIPLE CHOICE QUIZ TEST - CREATE NEW QUIZ ---------------");
+//        opencart.addQuiz();
+//
+//        System.out.println("--------------- MULTIPLE CHOICE QUIZ TEST - ATTEMPT TO ANSWER QUIZ ---------------");
+//        opencart.answerMultipleChoice(DEFAULT_LENGTH);
+//
+//        System.out.println("--------------- MULTIPLE CHOICE QUIZ TEST - TEACHER LOGIN ---------------");
+//        opencart.logoutAndCheck();
+    }
+}
---
+++ Cucumber/src/test/java/TestRunner.java
@@ -0,0 +1,14 @@
+////package hellocucumber;
+//import org.junit.runner.RunWith;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src\\test\\java\\hellocucumber"
+//)
+//
+//public class TestRunner {
+//
+//}
